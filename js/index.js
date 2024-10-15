ce1e3a08a3dab760a528bc5ee586e86bb4d70340
fr1ddz <48515346+ridzhack@users.noreply.github.com>
2024-10-15 15:49:08 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'CILL, I Have Something 4 U'.split('')
+const text = 'CILL, I Have Something'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
